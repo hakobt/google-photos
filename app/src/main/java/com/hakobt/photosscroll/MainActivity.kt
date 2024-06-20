c6f2b787f1bf82e01a169c9779f3e17abffd4a8f
Hakob <hakobtovmasyan19@gmail.com>
2024-06-20 17:51:06 +0400

    add photo grid

--- app/src/main/java/com/hakobt/photosscroll/MainActivity.kt
+++ app/src/main/java/com/hakobt/photosscroll/MainActivity.kt
@@ -11,20 +11,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridScope
+import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -43,8 +49,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.hakobt.photosscroll.ui.theme.PhotoScrollTheme
@@ -73,32 +82,61 @@ fun PhotoHomeScreen(paddingValues: PaddingValues) {
             .fillMaxSize()
             .padding(paddingValues)
     ) {
-        PhotoScrollView(photos = images + images + images + images)
-        PhotoGridView(photos = images + images + images + images)
+        val lazyStaggeredGridState = rememberLazyStaggeredGridState()
+//        PhotoScrollView(photos = images + images + images + images, lazyStaggeredGridState.firstVisibleItemScrollOffset)
+        PhotoGridView(photos = images + images + images + images + images, lazyStaggeredGridState)
     }
 }
 
 @Composable
-fun PhotoGridView(photos: List<String>) {
+fun PhotoGridView(photos: List<String>, lazyStaggeredGridState: LazyStaggeredGridState) {
     val gridType = remember {
         mutableStateOf(StaggeredGridCells.Fixed(5))
     }
     LazyVerticalStaggeredGrid(
         modifier = Modifier.padding(vertical = 24.dp),
         columns = gridType.value,
+        state = lazyStaggeredGridState,
         horizontalArrangement = Arrangement.spacedBy(2.dp),
         verticalItemSpacing = 2.dp
     ) {
-        items(photos) {
-            AsyncImage(
-                modifier = Modifier
-                    .clipToBounds()
-                    .aspectRatio(1f),
-                model = it,
-                contentScale = ContentScale.Crop,
-                contentDescription = ""
+        item(span = StaggeredGridItemSpan.FullLine) {
+            PhotoScrollView(photos = photos)
+        }
+
+        SectionView("Header 1", photos)
+        SectionView("Header 2", photos)
+        SectionView("Header 3", photos)
+        SectionView("Header 4", photos)
+    }
+}
+
+fun LazyStaggeredGridScope.SectionView(
+    header: String,
+    items: List<String>
+) {
+    item(span = StaggeredGridItemSpan.FullLine) {
+        Column {
+            Spacer(modifier = Modifier.padding(top = 48.dp))
+            Text(
+                modifier = Modifier.padding(
+                    vertical = 16.dp,
+                    horizontal = 8.dp
+                ),
+                text = header,
+                style = TextStyle(color = Color.Black, fontSize = TextUnit(24f, TextUnitType.Sp))
             )
         }
+    }
+    items(items) {
+        AsyncImage(
+            modifier = Modifier
+                .clipToBounds()
+                .aspectRatio(1f),
+            model = it,
+            contentScale = ContentScale.Crop,
+            contentDescription = ""
+        )
     }
 }
 
@@ -109,6 +147,7 @@ fun PhotoScrollView(photos: List<String>) {
     val visibleItems by remember {
         snapshotFlow {
             val layoutInfo = state.layoutInfo
+            layoutInfo.viewportStartOffset
             layoutInfo.visibleItemsInfo.mapIndexed { index, it ->
                 val cutFromLeft = (layoutInfo.viewportStartOffset - it.offset).coerceAtLeast(0)
                 val cutFromRight =
@@ -124,7 +163,6 @@ fun PhotoScrollView(photos: List<String>) {
         state = state,
         verticalAlignment = Alignment.Top
     ) {
-        stickyHeader {  }
         itemsIndexed(photos, { index, item -> index }) { index, item ->
             val firstVisibleItem = visibleItems.firstOrNull { it.first.key == index }
 
@@ -160,14 +198,10 @@ fun PhotoView(
             .padding(horizontal = 8.dp)
             .height(160.dp)
             .width(120.dp)
-            .clickable {
-                blurAmount.floatValue = 0f
-            }
             .clip(RoundedCornerShape(16.dp)),
     ) {
         AsyncImage(
             modifier = Modifier
-                .blur(blurAmount.floatValue.dp)
                 .height(160.dp)
                 .width(360.dp)
                 .absoluteOffset(-((cutFromRight - cutFromLeft) * 0.1).dp),
@@ -192,5 +226,4 @@ fun PhotoView(
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PhotoScrollView(photos = images)
 }
